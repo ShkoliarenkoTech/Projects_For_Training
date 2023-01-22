3b4daeba4a2ccb49f2b12d65e17c212b7e1b3e89
Dima <shkoliarenkod@gmail.com>
2023-01-22 18:16:40 +0100

    Static
    Static methods and class fields. Created the new solution in which have written the class to compare static class fields and common ones without "static" word

---
+++ Project/Static methods and class fields/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_methods_and_class_fields
+{
+   
+    class Count
+    {
+        public Count()
+        {
+            counter++;
+        }
+
+        private static int counter;
+
+        public static int Counter
+        {
+            get { return counter; }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Count count1 = new Count();
+            Count count2 = new Count();
+            Count count3 = new Count();
+
+            Console.WriteLine(Count.Counter);
+            Console.Read();
+        }
+    }
+}
