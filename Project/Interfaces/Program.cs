8f8be3f7aca03f0229bcc59f03d2c30e2b4597b5
Dima <shkoliarenkod@gmail.com>
2023-01-30 22:18:32 +0100

    Interfaces
    Have written 2 interfaces with the equal named methods and have seen the sequence which one will be called firstly

---
+++ Project/Interfaces/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+
+    interface IFirstInterface
+    {
+        void Function();
+    }
+    interface ISecondInterface
+    {
+        void Function();
+    }
+
+    class TestClass :ISecondInterface , IFirstInterface
+    {
+        void IFirstInterface.Function()
+        {
+            Console.WriteLine("It's the firstInterface function");
+        }
+
+        void ISecondInterface.Function()
+        {
+            Console.WriteLine("It's the secondInterface function");
+        }
+    }
+
+
+    class Program
+    {
+
+        static void Foo(IFirstInterface firstInterface)
+        {
+            firstInterface.Function();
+        }
+
+        static void Foo_second(ISecondInterface secondInterface)
+        {
+            secondInterface.Function();
+        }
+
+
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+
+
+            if (testClass is ISecondInterface secondInterface)
+            {
+                secondInterface.Function();
+                Console.WriteLine();
+            }
+            else if (testClass is IFirstInterface firstInterface)
+            {
+                firstInterface.Function();
+                Console.WriteLine();
+            }
+
+            Console.Read();
+
+
+        }
+    }
+}
