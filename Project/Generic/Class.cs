171bbb570affceade9e5a3f1e21df8b2eb2744a9
Dima <shkoliarenkod@gmail.com>
2023-02-01 21:47:55 +0100

    Generic type
    Today I`ve studied generic type of data and have written the class like method Generic

---
+++ Project/Generic/Class.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generic;
+namespace Generic
+{
+    class ClassMyList<T>
+    {
+        private T[] _Array = Array.Empty<T>();
+
+        public T this[int index]
+            {
+                get
+                {
+                     return _Array[index];
+                }
+                set
+                {
+                _Array[index] = value;
+                }
+
+            } 
+
+        public int Count { get { return _Array.Length; } }
+
+        public void Add(T value)
+
+        {
+            var newArray = new T [_Array.Length + 1];
+            for (int i = 0; i < _Array.Length; i++)
+            {
+                newArray[i] = _Array[i];
+            }
+            newArray[_Array.Length] = value;
+            _Array = newArray;
+        }
+        
+    }
+}
---
+++ Project/Generic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generic;
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassMyList<int> MyList = new ClassMyList<int>();
+            MyList.Add(5);
+            int count = MyList.Count;
+        }
+    }
+}
